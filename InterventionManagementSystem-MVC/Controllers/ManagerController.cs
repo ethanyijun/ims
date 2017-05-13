ce071466ecbb2edbf0c0a427cc905112b6be5b2e
Yuan Chai <11998402@student.uts.edu.au>
2017-05-13 15:33:17 +1000

    Add a home page for Manager

---
+++ InterventionManagementSystem-MVC/Controllers/ManagerController.cs
@@ -0,0 +1,33 @@
+﻿using IMSLogicLayer.Services;
+using IMSLogicLayer.ServiceInterfaces;
+using InterventionManagementSystem_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
+namespace InterventionManagementSystem_MVC.Controllers
+{
+    public class ManagerController : Controller
+    {
+        // GET: Manager
+        public ActionResult Index()
+        {
+            var identityId = User.Identity.GetUserId();
+
+            IManagerService manager = new ManagerService("2b8dbe21-cc7b-4794-bf3e-4a2d3a7b68e0");
+            var user = manager.GetDetail();
+            var model = new ManagerViewModels()
+            {
+                Name = user.Name,
+                DistrictName = user.District.Name,
+                AuthorisedCosts = user.AuthorisedCosts,
+                AuthorisedHours = user.AuthorisedHours
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ InterventionManagementSystem-MVC/Models/ManagerViewModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InterventionManagementSystem_MVC.Models
+{
+    public class ManagerViewModels
+    {
+        public string Name { get; set; }
+        public string DistrictName { get; set; }
+        public Nullable<decimal> AuthorisedHours { get; set; }
+        public Nullable<decimal> AuthorisedCosts { get; set; }
+    }
+}
