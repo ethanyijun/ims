ca7bd2a04c0bf0f50a3b544791064c01a96c03a2
Yuan Chai <11998402@student.uts.edu.au>
2017-05-13 14:20:08 +1000

    Generate model from database

---
+++ IMSDBLayer/Models/ReportRow.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMSDBLayer.Models
+{
+    public partial class ReportRow
+    {
+        private string firstProperty;
+
+        private decimal? costs;
+
+        private decimal? hours;
+        public ReportRow() { }
+
+        public string FirstProperty
+        {
+            get
+            {
+                return firstProperty;
+            }
+
+            set
+            {
+                firstProperty = value;
+            }
+        }
+
+        public decimal? Costs
+        {
+            get
+            {
+                return costs;
+            }
+
+            set
+            {
+                costs = value;
+            }
+        }
+
+        public decimal? Hours
+        {
+            get
+            {
+                return hours;
+            }
+
+            set
+            {
+                hours = value;
+            }
+        }
+    }
+}
