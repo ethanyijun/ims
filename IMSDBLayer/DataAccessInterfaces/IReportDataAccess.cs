8a9e5aec67fb8c225975dba08c7ad6be34ce1c8f
Yuan Chai <11998402@student.uts.edu.au>
2017-05-13 14:23:18 +1000

    Copy interfaces from assignment 1

---
+++ IMSDBLayer/DataAccessInterfaces/IReportDataAccess.cs
@@ -0,0 +1,34 @@
+﻿using IMSDBLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMSDBLayer.DataAccessInterfaces
+{
+    public interface IReportDataAccess
+    {
+        /// <summary>
+        /// Get report data from database
+        /// </summary>
+        /// <returns>A list of report data</returns>
+        IEnumerable<ReportRow> totalCostByEngineer();
+        /// <summary>
+        /// Get report data from database
+        /// </summary>
+        /// <returns>A list of report data</returns>
+        IEnumerable<ReportRow> averageCostByEngineer();
+        /// <summary>
+        /// Get report data from database
+        /// </summary>
+        /// <returns>A list of report data</returns>
+        IEnumerable<ReportRow> costByDistrict();
+        /// <summary>
+        /// Get report data from database
+        /// </summary>
+        /// <returns>A list of report data</returns>
+        IEnumerable<ReportRow> monthlyCostForDistrict(Guid districtId);
+
+    }
+}
