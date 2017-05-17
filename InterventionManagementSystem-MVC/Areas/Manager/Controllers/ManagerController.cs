c2ced4061ac4bfa4681dd996441c1c9fb266037b
Yuan Chai <11998402@student.uts.edu.au>
2017-05-17 20:01:21 +1000

    using Area

--- InterventionManagementSystem-MVC/Areas/Manager/Controllers/ManagerController.cs
+++ InterventionManagementSystem-MVC/Areas/Manager/Controllers/ManagerController.cs
@@ -1,23 +1,21 @@
-﻿using IMSLogicLayer.Services;
-using IMSLogicLayer.ServiceInterfaces;
+﻿using IMSLogicLayer.ServiceInterfaces;
+using IMSLogicLayer.Services;
 using InterventionManagementSystem_MVC.Models;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Microsoft.AspNet.Identity;
 
-namespace InterventionManagementSystem_MVC.Controllers
+namespace InterventionManagementSystem_MVC.Areas.Manager.Controllers
 {
     public class ManagerController : Controller
     {
-        // GET: Manager
+        // GET: Manager/Manager
         public ActionResult Index()
         {
-            var identityId = User.Identity.GetUserId();
-
-            IManagerService manager = new ManagerService("2b8dbe21-cc7b-4794-bf3e-4a2d3a7b68e0");
+            IManagerService manager = getManagerService();
             var user = manager.GetDetail();
             var model = new ManagerViewModel()
             {
@@ -26,74 +24,63 @@ namespace InterventionManagementSystem_MVC.Controllers
                 AuthorisedCosts = user.AuthorisedCosts,
                 AuthorisedHours = user.AuthorisedHours
             };
-
             return View(model);
         }
-        [HttpPost]
-        public ActionResult ApprovedList(FormCollection form)
-        {
 
+        public ActionResult InterventionList()
+        {
+            IManagerService manager = getManagerService();
+            var interventionList = manager.GetApprovedInterventions();
+            var interventions = new List<InterventionViewModel>();
+            var viewList = new List<SelectListItem>()
+            {
+                new SelectListItem(){ Text ="Approved",Value="Approved", Selected=true },
+                new SelectListItem(){ Text="Proposed", Value="Proposed"}
+            };
+            bindIntervention(interventionList, interventions);
+            var model = new ManagerViewInterventionModel() { ViewList = viewList, Interventions = interventions };
+            return View(model);
+        }
+
+     
+
+        public ActionResult InterventionList(FormCollection form)
+        {
             if (form["SelectedType"].ToString().Equals("Approved"))
             {
                 return View();
-            }else
+            }
+            else
             {
                 var identityId = User.Identity.GetUserId();
-
                 IManagerService manager = new ManagerService("2b8dbe21-cc7b-4794-bf3e-4a2d3a7b68e0");
                 var user = manager.GetDetail();
                 var interventionList = manager.GetInterventionsByState(IMSLogicLayer.Enums.InterventionState.Proposed);
                 var interventions = new List<InterventionViewModel>();
-
                 var viewList = new List<SelectListItem>()
-            {
-                new SelectListItem(){ Text ="Approved",Value="Approved"},
-                new SelectListItem(){ Text="Proposed", Value="Proposed",Selected=true }
-            };
-
-
-                foreach (var intervention in interventionList)
                 {
-                    interventions.Add(new InterventionViewModel()
-                    {
-                        InterventionTypeName = intervention.InterventionType.Name,
-                        ClientName = intervention.Client.Name,
-                        DateCreate = intervention.DateCreate,
-                        InterventionState = intervention.InterventionState.ToString(),
-                        DistrictName = intervention.District.Name,
-                        Costs = intervention.Costs,
-                        Hours = intervention.Hours,
-                        DateFinish = intervention.DateFinish,
-                        Comments = intervention.Comments
-
-                    });
-                }
-
-
+                    new SelectListItem(){ Text ="Approved",Value="Approved"},
+                    new SelectListItem(){ Text="Proposed", Value="Proposed",Selected=true }
+                };
+                bindIntervention(interventionList, interventions);
                 var model = new ManagerViewInterventionModel() { ViewList = viewList, Interventions = interventions };
                 return View(model);
-            }
-           
 
-            
+            }
+
         }
 
-        public ActionResult ApprovedList()
+        private IManagerService getManagerService()
         {
             var identityId = User.Identity.GetUserId();
 
             IManagerService manager = new ManagerService("2b8dbe21-cc7b-4794-bf3e-4a2d3a7b68e0");
-            var user = manager.GetDetail();
-            var interventionList = manager.GetApprovedInterventions();
-            var interventions = new List<InterventionViewModel>();
 
-            var viewList = new List<SelectListItem>()
-            {
-                new SelectListItem(){ Text ="Approved",Value="Approved"},
-                new SelectListItem(){ Text="Proposed", Value="Proposed"}
-            };
+            return manager;
+        }
 
-
+        private void bindIntervention(IEnumerable<IMSLogicLayer.Models.Intervention> interventionList, List<InterventionViewModel> interventions)
+        {
             foreach (var intervention in interventionList)
             {
                 interventions.Add(new InterventionViewModel()
@@ -110,11 +97,6 @@ namespace InterventionManagementSystem_MVC.Controllers
 
                 });
             }
-
-
-            var model = new ManagerViewInterventionModel() { ViewList = viewList, Interventions = interventions };
-            return View(model);
         }
-
     }
 }
---
+++ InterventionManagementSystem-MVC/Areas/SiteEngineer/SiteEngineerAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace InterventionManagementSystem_MVC.Areas.SiteEngineer
+{
+    public class SiteEngineerAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "SiteEngineer";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "SiteEngineer_default",
+                "SiteEngineer/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
