638144479926727fad9cc33052a2b68feeb83c29
Po <colagarychen@gmail.com>
2017-05-24 16:54:08 +1000

    ManagerControllerTest testmethods init

--- InterventionManagementSystem-MVC.Tests/Controllers/ManagerControllerTest.cs
+++ InterventionManagementSystem-MVC.Tests/Controllers/ManagerControllerTest.cs
@@ -1,14 +1,57 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InterventionManagementSystem_MVC.Areas.Manager.Controllers;
+using InterventionManagementSystem_MVC.Areas.Manager.Models;
+using System.Web.Mvc;
 
 namespace InterventionManagementSystem_MVC.Tests.Controllers
 {
     [TestClass]
     public class ManagerControllerTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        private ManagerController controller;
+
+        [TestInitialize]
+        public void SetUp()
         {
+            controller = new ManagerController();
+        }
+        [TestMethod]
+        public void TestIndexView()
+        {
+            var view = controller.Index() as ViewResult;
+
+            Assert.AreEqual("Index", view.ViewName);
+        }
+
+        [TestMethod]
+        public void TestIndexViewModel()
+        {
+            var view = controller.Index() as ViewResult;
+            var model = (ManagerViewModel)view.ViewData.Model;
+
+            Assert.IsNotNull(model.Name);
+            Assert.IsNotNull(model.DistrictName);
+            Assert.IsNotNull(model.AuthorisedCosts);
+            Assert.IsNotNull(model.AuthorisedHours);
+        }
+
+        [TestMethod]
+        public void TestInterventionListView()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestInterventionListViewModel()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestInterventionListViewPost()
+        {
+
         }
     }
 }
