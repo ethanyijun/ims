b06996088f650be68afac61530cdf324e6962590
Po <colagarychen@gmail.com>
2017-05-24 13:54:05 +1000

    Controller unittest init

---
+++ InterventionManagementSystem-MVC.Tests/Controllers/ManagerControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InterventionManagementSystem_MVC.Tests.Controllers
+{
+    [TestClass]
+    public class ManagerControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ InterventionManagementSystem-MVC.Tests/Controllers/SiteEngineerControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InterventionManagementSystem_MVC.Areas.SiteEngineer.Controllers;
+using System.Web.Mvc;
+
+namespace InterventionManagementSystem_MVC.Tests.Controllers
+{
+    [TestClass]
+    public class SiteEngineerControllerTest
+    {
+        //[TestMethod]
+        //public void TestIndexView()
+        //{
+        //    var controller = new SiteEngineerController();
+        //    var view = controller.Index() as ViewResult;
+        //    Assert.AreEqual("Index", view.ViewName);
+        //}
+
+
+    }
+}
