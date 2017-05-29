84beaca9f4539a06020df62e0a1979d514337024
Yuan Chai <11998402@student.uts.edu.au>
2017-05-29 20:39:37 +1000

    IEmail service created for sending notification

---
+++ IMSLogicLayer/ServiceInterfaces/IEmailService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace IMSLogicLayer.ServiceInterfaces
+{
+    public interface IEmailService
+    {
+        MailMessage CreateMessage(string FromAddress,string ToAddress, string UserFrom, string UserTo, Models.Intervention intervention);
+
+        void SendEmail(MailMessage message);
+
+    }
+}
---
+++ IMSLogicLayer/Services/EmailService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using IMSLogicLayer.Models;
+
+namespace IMSLogicLayer.Services
+{
+    public class EmailService : ServiceInterfaces.IEmailService
+    {
+        public MailMessage CreateMessage(string FromAddress, string ToAddress, string UserFrom, string UserTo,Intervention intervention)
+        {
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress("FromAddress");
+            message.To.Add(ToAddress);
+            message.Subject = "Approval Notification";
+
+            message.Body = UserFrom + "has approved the intervention :" + intervention.InterventionType + " create by " + UserTo + " on " + DateTime.Now;
+            return message;
+        }
+
+        public void SendEmail(MailMessage message)
+        {
+            using (SmtpClient client = new SmtpClient())
+            {
+                client.Send(message);
+            }
+        }
+    }
+}
